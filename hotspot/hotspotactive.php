d6c3e5e723486b14db82c5481b1b68e93116290e
kakanet <52841812+kakanet@users.noreply.github.com>
2021-03-05 09:27:35 +0800

    Update hotspotactive.php

--- hotspot/hotspotactive.php
+++ hotspot/hotspotactive.php
@@ -801,7 +801,7 @@ $rsj = $API->comm("/interface/print");  //
   <tr>
     <th bgcolor="orange" class="text-center">Nama</th>
     <th bgcolor="orange" class="text-center">Pemakaian</th>
-    <th bgcolor="orange" class="text-center">Uptime</th>
+    <th bgcolor="orange" class="text-center">Uptime | Comment</th>
   </tr>
   </thead>
   <tbody>
@@ -835,7 +835,7 @@ for ($i = 0; $i < $TotalReg; $i++) {
                       " . $x['hapus']; 
                     ?></td>
 	<td bgcolor=<?php if($rrr < "1"){ echo $bgcolor = "black"; }elseif($rrr < "1073741825"){ echo $bgcolor = "Gainsboro"; }elseif($rrr > "1073741824"){ echo $bgcolor = "yellow"; }?> style=<?php if($rrr < "1"){ echo $style = "display:none"; }elseif($rrr > "0"){ echo $style = "text-align:center"; }?>><?php 
-                    echo "".$uptime." / ".$comment."
+                    echo "".$uptime." | ".$comment."
                       " . $x['hapus']; 
                     ?></td>
 <?php
@@ -930,7 +930,7 @@ if (!isset($_SESSION["mikhmon"])) {
     </th>
     <th bgcolor="orange" class="text-center">Nama</th>
     <th bgcolor="orange" class="text-center">Pemakaian</th>
-    <th bgcolor="orange" class="text-center">Uptime</th>
+    <th bgcolor="orange" class="text-center">Uptime | Comment</th>
   </tr>
   </thead>
   <tbody>
@@ -963,7 +963,7 @@ for ($i = 0; $i < $TotalReg; $i++) {
                       " . $x['hapus']; 
                     ?></td>
 	<td bgcolor=<?php if($pemakaianasli < "1"){ echo $bgcolor = "black"; }elseif($pemakaianasli < "1073741825"){ echo $bgcolor = "Gainsboro"; }elseif($pemakaianasli > "1073741824"){ echo $bgcolor = "yellow"; }?> style=<?php if($pemakaianasli < "1"){ echo $style = "display:none"; }elseif($pemakaianasli > "0"){ echo $style = "text-align:center"; }?>><?php 
-                    echo "".$uptime." / ".$comment."
+                    echo "".$uptime." | ".$comment."
                       " . $x['hapus']; 
                     ?></td>
 	<?php echo "</tr>";
